e9a9584f4b196cf4cfdb417f6e20444f39bc8fcd
Cristian Sotomayor <cristiansotomayor.dev@gmail.com>
2021-01-21 00:08:26 -0500

    quitando nombres

--- angular-material-boilerplate/e2e/src/app.e2e-spec.ts
+++ angular-material-boilerplate/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('angular-mat-example app is running!');
+    expect(await page.getTitleText()).toEqual('angular-material-boilerplate app is running!');
   });
 
   afterEach(async () => {
--- angular-material-boilerplate/src/app/app.component.spec.ts
+++ angular-material-boilerplate/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-mat-example'`, () => {
+  it(`should have as title 'angular-material-boilerplate'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-mat-example');
+    expect(app.title).toEqual('angular-material-boilerplate');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-mat-example app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('angular-material-boilerplate app is running!');
   });
 });

f0617d16df1226f82e0ee80f671dc78f5601db9a
Cristian Sotomayor <cristiansotomayor.dev@gmail.com>
2021-01-21 00:23:26 -0500

    cambios para el auth

---
+++ angular-material-boilerplate/src/app/auth/auth.module.ts
@@ -0,0 +1,15 @@
+// Importaciones basicas
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+// Aqui pondremos todos los modulos de material que usaremos
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+  ]
+})
+export class AuthModule { }

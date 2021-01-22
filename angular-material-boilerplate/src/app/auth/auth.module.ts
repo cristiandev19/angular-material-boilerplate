a54d003fbcb3a00ac99d95e1fc545bb9d908f744
Cristian Sotomayor <cristiansotomayor.dev@gmail.com>
2021-01-21 23:18:37 -0500

    modulo del auth

--- angular-material-boilerplate/src/app/auth/auth.module.ts
+++ angular-material-boilerplate/src/app/auth/auth.module.ts
@@ -1,13 +1,22 @@
 // Importaciones basicas
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AuthRoutingModule } from './auth-routing.module';
+import { MaterialModule } from '../material/material.module';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 
 // Aqui pondremos todos los modulos de material que usaremos
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginComponent,
+    RegisterComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    AuthRoutingModule,
+    MaterialModule
   ],
   exports: [
   ]
